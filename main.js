85a80ad61853934ab0d6446d80bead950bafeebc
Ben <ben.walcutt@gmail.com>
2019-11-07 20:07:44 -0600

    files from nov 7

--- main.js
+++ main.js
@@ -46,14 +46,104 @@ var posts = [
     }
 ]
 
+function drawPosts() {
+    $('#content').html('');
+    posts.forEach(p => drawPost(p));
+}
+
 function drawPost(post) {
     var element = document.createElement('post');
     element.id = 'post' + post.id;
-    element.innerHTML = '<div class="post-title">' + post.title + '</div>';
-    element.innerHTML += '<div class="post-text">' + post.text.substring(0, 100) + '</div>';
-    element.innerHTML += '<div class="post-author">' + post.author + '</div>';
-    element.style.display = 'flex';
-    element.style.flexDirection = 'column';
 
+    element.innerHTML = printFullHTML(post);
     $('#content').append(element);
+}
+
+function likePost(id) {
+    let post = posts.find(p => p.id === id);
+    post.likes++;
+    $('#up' + id).addClass('red');
+    drawPosts();
+}
+
+function dislikePost(id) {
+    let post = posts.find(p => p.id === id);
+    post.dislikes++;
+    $('#down' + id).addClass('blue');
+    drawPosts();
+}
+
+function printFullHTML(post) {
+    var fullHTML = '<div class="flex row-container">';
+    fullHTML += printLeftHTML(post);
+    fullHTML += printRightHTML(post);
+    fullHTML += '</div>';
+    return fullHTML;
+}
+
+function printLeftHTML(post) {
+    var tempHTML = '<div class="flex col-container p20" style="justify-content: space-evenly; border: 1px solid gray">';
+    tempHTML += printThumbsUp(post);
+    tempHTML += printCount(post);
+    tempHTML += printThumbsDown(post);
+    tempHTML += '</div>';
+    return tempHTML;
+}
+
+function printThumbsUp(post) {
+    var tempHTML = '<div id="up' + post.id + '" onclick="likePost(' + post.id + ')">';
+    tempHTML += '<i class="far fa-thumbs-up"></i>';
+    tempHTML += '</div>';
+    return tempHTML;
+}
+
+function printCount(post) {
+    return post.likes - post.dislikes;
+}
+
+function printThumbsDown(post) {
+    var tempHTML = '<div id="down' + post.id + '" onclick="dislikePost(' + post.id + ')">';
+    tempHTML += '<i class="far fa-thumbs-down"></i>';
+    tempHTML += '</div>';
+    return tempHTML;
+}
+
+function printRightHTML(post) {
+    var tempHTML = '<div class="flex col-container" style="width: 100%">';
+    tempHTML += printTitle(post.title);
+    tempHTML += printText(post.text);
+    tempHTML += printAuthor(post.author);
+    tempHTML += '</div>';
+    return tempHTML;
+}
+
+function printTitle(title) {
+    return '<div class="post-title">' + title + '</div>'
+}
+
+function printText(text) {
+    return '<div class="post-text">' + text.substring(0, 100) + '</div>';
+}
+
+function printAuthor(author) {
+    return '<div class="post-author">' + author + '</div>';
+}
+
+function createNewPost() {
+    let title = $('#postTitle').val();
+    let text = $('#postText').val();
+    let subraddit = $('#postSubraddit').val();
+
+    let newPost = {
+        id: posts.length,
+        title: title,
+        text: text,
+        subraddit: subraddit,
+        author: 'user@test.com',
+        likes: 0,
+        dislikes: 0,
+    }
+
+    posts.push(newPost);
+    drawPosts();
 }
